bd2becae076a12d76784c4b87f5ef9fcf75bf4f6
IDLeoMAX <87306272+IDLeoMAX@users.noreply.github.com>
2021-07-12 18:56:18 +0800

    Update bot.py

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord
+from discord import channel
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='a!')
+
+@bot.event
+async def on_ready():
+    print(">> bot is online <<")
+
+@bot.event
+async def on_member_join(member):
+    channel = bot.get_all_channel(769876063839846401)
+    await channel.send(f'{member} join!')
+
+bot.run('ODYzODY4NjM4MDI5NDE0NDAx.YOtKXQ.KH0NwEyE0eg4m095CgG_10VFLQo')
